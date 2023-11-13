67978a051de4801960cabcdda33766418a572d94
JordanDestin <destin.jord@gmail.com>
2023-11-13 21:24:38 +0100

    initialize project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/auth/LoginScreen';
+import RegisterScreen from './screens/auth/RegisterScreen';
+import ListThemeScreen from './screens/theme/ListThemeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <RootNavigator />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AuthStack() {
+  return(
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
+  )
+}
+
+function ListThemeStack() {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="ListTheme" component={ListThemeScreen} />
+    </Stack.Navigator>
+  )
+}
+
+function RootNavigator() {
+  return(
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  )
+  
+}
+
+
