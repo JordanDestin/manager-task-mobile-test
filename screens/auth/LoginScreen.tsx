9c67f6719da3de4964f5f0fb0928b6c49e9c64fa
JordanDestin <destin.jord@gmail.com>
2023-11-14 17:12:56 +0100

    up

--- screens/auth/LoginScreen.tsx
+++ screens/auth/LoginScreen.tsx
@@ -1,9 +1,54 @@
-import { Text } from 'react-native';
+import { Text,View, TextInput, StyleSheet, Button } from 'react-native';
 
-export default function LoginScreen() {
+export default function LoginScreen({navigation}) {
     return(
-     <>
-     <Text>LoginScreen</Text>
-     </>
+        <View style={styles.container}>
+            <Text style ={styles.title}>Se connecter</Text>
+            <TextInput 
+                style={styles.input} 
+                keyboardType='email-address'
+                placeholder='addresse mail'/>
+            <TextInput 
+                style={styles.input} 
+                autoCorrect={false}
+                secureTextEntry={true}
+                textContentType='password'
+                placeholder='mot de passe'/>
+                <Button 
+                    title='Se connecter' 
+                    onPress={()=>navigation.navigate('ListTheme')}/>
+                <Text>S'inscrire</Text>
+                <Button color="gray" 
+                    title="S'inscire" 
+                    onPress={()=>navigation.navigate('Register')}/>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container :{
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 50,
+        paddingHorizontal: 12
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#333',
+        borderRadius: 8,
+        padding: 12,
+        marginBottom: 20
+    },
+    text:{
+        fontSize:18,
+        padding:12
+
+    },
+    title:{
+        fontSize: 24,
+        fontWeight: '600',
+        color: '#444',
+        alignSelf: 'center',
+        paddingBottom: 24,
+    }
+})
--- screens/auth/RegisterScreen.tsx
+++ screens/auth/RegisterScreen.tsx
@@ -1,10 +1,60 @@
-import { Text } from 'react-native';
+import { Text,View, TextInput, StyleSheet, Button } from 'react-native';
 
-
-export default function RegisterScreen() {
+export default function RegisterScreen({navigation}) {
     return(
-     <>
-     <Text>RegisterScreen</Text>
-     </>
+        <View style={styles.container}>
+            <Text style ={styles.title}>S'inscrire'</Text>
+            <TextInput 
+                style={styles.input} 
+                placeholder='Nom'
+                />
+            <TextInput 
+                style={styles.input} 
+                keyboardType='email-address'
+                placeholder='addresse mail'
+                />
+            <TextInput 
+                style={styles.input} 
+                autoCorrect={false}
+                secureTextEntry={true}
+                textContentType='password'
+                placeholder='mot de passe'
+                />
+                <Button 
+                    title='Enregistrer' 
+                    onPress={()=>navigation.navigate('ListTheme')}/>
+                <Text>Se connecter</Text>
+                <Button color="gray" 
+                    title="Se connecter" 
+                    onPress={()=>navigation.navigate('Login')}/>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container :{
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 50,
+        paddingHorizontal: 12
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#333',
+        borderRadius: 8,
+        padding: 12,
+        marginBottom: 20
+    },
+    text:{
+        fontSize:18,
+        padding:12
+
+    },
+    title:{
+        fontSize: 24,
+        fontWeight: '600',
+        color: '#444',
+        alignSelf: 'center',
+        paddingBottom: 24,
+    }
+})
